1edfc855acf6d823686145ed0d6a2ee2d1e46c7e
Mask-of-the-Fractal-Abyss <44147428+Mask-of-the-Fractal-Abyss@users.noreply.github.com>
2019-07-07 17:02:02 -0400

    Add files via upload

--- daoFramework.sol
+++ daoFramework.sol
@@ -1,17 +1,46 @@
 pragma solidity ^0.5.1;
 
 contract dao{
-  address owner = msg.sender;
-  mapping(address => uint256) public votes;
-  address payable addr = address(uint160(address(this)));
+  address[] public rules;
 
-  function sign() payable public {
-    require(msg.value > 9);
-    votes[msg.sender] = msg.value;
-    addr.transfer(msg.value);
+  function ammend(address old, address new_) payable public{
+    require(msg.value > 4);
+    for(uint i = 0; i < rules.length; i++){
+      if (rules[i] == old) {
+        rules[i] = new_;
+        break;
+      }
+    }
   }
 
-  function unsign() payable public {
-    delete votes[msg.sender];
+  function add(address addition) payable public{
+    require(msg.value > 9);
+    rules.push(addition);
+  }
+
+  function repeal(address target) public{
+    for(uint i = 0; i < rules.length; i++){
+      if (rules[i] == target){
+        delete rules[i];
+        break;
+      }
+    }
+  }
+}
+
+contract timedEvent{
+  uint reward;
+  uint time;
+  address payable addr = address(uint160(address(this)));
+  constructor(uint r, uint t) payable public{
+    require(r == msg.value);
+    addr.transfer(msg.value);
+    reward = r;
+    time = t;
+  }
+
+  function claim() public{
+    require(now >= time);
+    msg.sender.transfer(reward);
   }
 }
